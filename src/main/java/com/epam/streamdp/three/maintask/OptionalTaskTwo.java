0d1f9b2876a8f96e682bb66414d338a1865673e4
Alexander <59509850+streamdp@users.noreply.github.com>
2020-01-31 01:35:21 +0300

    Optional task two from the third package is completed.

--- src/main/java/com/epam/streamdp/three/maintask/OptionalTaskTwo.java
+++ src/main/java/com/epam/streamdp/three/maintask/OptionalTaskTwo.java
@@ -1,11 +1,42 @@
 package com.epam.streamdp.three.maintask;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class OptionalTaskTwo {
 //    2.   Ввести число, занести его цифры в стек. Вывести число, у которого цифры идут в обратном порядке.
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
+
+        System.out.println("Please, enter an Integer:");
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()){
+            System.out.println("Please, enter an correct Integer:");
+            scanner.next();
+        }
+
+        Integer integer = scanner.nextInt();
+        char[] charsArray = integer.toString().toCharArray();
+        List<Character> integerStack = new ArrayList<>(charsArray.length);
+
+        boolean wasItPositiveInteger = true;
+
+        for (int i = 0; i < charsArray.length ; i++) {
+            if (charsArray[i] != '-') {
+                integerStack.add(charsArray[i]);
+            } else {
+                wasItPositiveInteger = false;
+            }
+        }
+
+        Collections.reverse(integerStack);
+        StringBuilder integerString = new StringBuilder();
+
+        for (Character character:integerStack){
+            integerString.append(character.charValue());
+        }
+
+        System.out.println(wasItPositiveInteger? Integer.parseUnsignedInt(integerString.toString()) : Integer.parseInt(integerString.toString()) * -1);
     }
 }
