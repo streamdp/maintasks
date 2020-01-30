a36a41b3575c1183842797bf3e4c1614ee8e0686
Alexander Prymak <primak.aa@gmail.com>
2020-01-30 04:32:14 +0300

    test commit

--- src/main/java/com/epam/streamdp/three/maintask/OptionalTaskTwo.java
+++ src/main/java/com/epam/streamdp/three/maintask/OptionalTaskTwo.java
@@ -1,8 +1,12 @@
 package com.epam.streamdp.three.maintask;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OptionalTaskTwo {
 //    2.   Ввести число, занести его цифры в стек. Вывести число, у которого цифры идут в обратном порядке.
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        
+    }
 }
